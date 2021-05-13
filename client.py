6b08bea78f30341e36556e737f353fae5888343a
Victor Ng <victor@draper.ai>
2021-05-13 16:44:05 -0400

    Add basic asyncio client

---
+++ client.py
@@ -0,0 +1,21 @@
+import logging
+import asyncio
+from grpc import aio
+
+from fastr.proto_lib import helloworld_pb2
+from fastr.proto_lib import helloworld_pb2_grpc
+
+
+async def run():
+    # NOTE(gRPC Python Team): .close() is possible on a channel and should be
+    # used in circumstances in which the with statement does not fit the needs
+    # of the code.
+    async with aio.insecure_channel('localhost:50051') as channel:
+        stub = helloworld_pb2_grpc.GreeterStub(channel)
+        response = await stub.SayHello(helloworld_pb2.HelloRequest(name='you'))
+    print("Greeter client received: " + response.message)
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    asyncio.run(run())
